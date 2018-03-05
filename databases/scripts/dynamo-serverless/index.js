34a40a4d916feec5d60fa7b5172d249449559dbb
mittyo <dimitri+github@tarasowski.de>
2018-03-05 18:01:39 +0100

    Got local DB working

---
+++ databases/scripts/dynamo-serverless/index.js
@@ -0,0 +1,36 @@
+'use strict';
+const AWS = require('aws-sdk');
+
+AWS.config.update({
+    region: 'eu-central-1',
+    endpoint: 'http://172.16.123.1:8000'
+})
+
+const docClient = new AWS.DynamoDB.DocumentClient()
+
+exports.handler = (event, context, callback) => {
+
+    const table = 'Movies';
+    const year = 2015;
+    const title = 'The Big New Movie';
+    
+    const params = {
+        TableName: table,
+        Key: {
+            "year": year,
+            "title": title
+        }
+    };
+
+    docClient.get(params, (err, data) => {
+        if (err) {
+            console.error('Unable to read item. Error JSON', JSON.stringify(err, null, 2));
+        } else {
+            callback(null, {
+                statusCode: 200,
+                body: JSON.stringify(data)
+            })
+        }
+    });
+
+}
