3be04bd0fbccd66b21fd87236a3638d7f8a35039
mittyo <dimitri+github@tarasowski.de>
2018-03-19 06:38:56 +0100

    Design patterns

---
+++ patterns/dependency-injection/examples/mongo.js
@@ -0,0 +1,5 @@
+module.exports = {
+  query(field) {
+    return 'John Doe from MongoDB';
+  }
+}
---
+++ patterns/dependency-injection/examples/noname.js
@@ -0,0 +1,8 @@
+module.exports = {
+  query(field) {
+    return 'John Doe from NoNameDB';
+  },
+  logme(field) {
+    return 'Logging of errors here';
+  }
+}
