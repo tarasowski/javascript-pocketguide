3030ad61a0f0024f2991a2f1d2349aa0a5d26a67
Dimitri Tarasowski <33748291+tarasowski@users.noreply.github.com>
2019-01-19 15:25:45 +0100

    Create code-snippets.js

---
+++ mentorship/code-snippets.js
@@ -0,0 +1,6 @@
+const map = f => xs => xs.reduce((a, c) => a.concat([f(c)]), [])
+const filter = pred => xs => xs.reduce((a, c) => pred(c) => ? a.concat([c]) : a, [])
+const compose = (...fns) => x => fns.reduceRight((v, f) => f(v), x)
+const composeP = (...fns) => x => fns.reduceRight(async (v, f) => f(await v))
+const pipe = (...fns) => x => fns.reduce((v, f) => f(v), x)
+const pipeP = (...fns) => x => fns.reduce(async (v, f) => f(await v))
