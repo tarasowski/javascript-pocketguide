7f201637a2af8a1ab50bb28d85e4a71e2f9b9948
Dimitri Tarasowski <33748291+tarasowski@users.noreply.github.com>
2019-02-10 13:20:09 +0100

    Update ***_code-snippets.js

--- mentorship/***_code-snippets.js
+++ mentorship/***_code-snippets.js
@@ -1,9 +1,7 @@
 const map = f => xs => xs.reduce((a, c) => a.concat([f(c)]), [])
 const filter = pred => xs => xs.reduce((a, c) => pred(c) => ? a.concat([c]) : a, [])
 const compose = (...fns) => x => fns.reduceRight((v, f) => f(v), x)
-const composeP = (...fns) => x => fns.reduceRight(async (v, f) => f(await v))
-const pipe = (...fns) => x => fns.reduce((v, f) => f(v), x)
-const pipeP = (...fns) => x => fns.reduce(async (v, f) => f(await v))
+const composeP = (...fns) => x => fns.reduceRight(async (v, f) => f(await v), x)
 const head = xs => xs[0]
 const reverse = reduce((a, x) => [x].concat(a), [])
 const last = compose(head, reverse)
