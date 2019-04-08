5069d1fa184828aa6660bc83d6eb12e8764a178a
Dimitri Tarasowski <33748291+tarasowski@users.noreply.github.com>
2019-04-08 07:50:34 +0200

    Delete adapter.js

--- patterns/dependency-injection/examples/adapter.js
+++
@@ -1,8 +0,0 @@
-module.exports = function(diDb) {
-  // here we suppose that diDb has query method but AWS for example name it as select
-  return {
-    query(field) {
-      return diDb.select(field);
-    }
-  }
-}
